4f5df4d8500d2a85963f6aa1594f42282be95de4
Fanny <lifangning1234@yahoo.com>
2020-11-25 18:51:24 -0500

    save db table data script, refactor linux scripts

---
+++ db_script/catalog_1_2_3_data.sql
@@ -0,0 +1,26 @@
+# catalog_1 table data
+insert into catalog_1 values('1', 'phone');
+insert into catalog_1 values('2', 'computer');
+insert into catalog_1 values('3', 'book');
+
+# catalog_2 table data
+insert into catalog_2 values('1', 'phone1', '1');
+insert into catalog_2 values('2', 'phone2', '1');
+insert into catalog_2 values('3', 'computer1', '2');
+insert into catalog_2 values('4', 'computer2', '2');
+insert into catalog_2 values('5', 'book1', '3');
+insert into catalog_2 values('6', 'book2', '3');
+
+# catalog_3 table data
+insert into catalog_3 values('1', 'phone1_1', '1');
+insert into catalog_3 values('2', 'phone1_2', '1');
+insert into catalog_3 values('3', 'phone2_1', '2');
+insert into catalog_3 values('4', 'phone2_2', '2');
+insert into catalog_3 values('5', 'computer1_1', '3');
+insert into catalog_3 values('6', 'computer1_2', '3');
+insert into catalog_3 values('7', 'computer2_1', '4');
+insert into catalog_3 values('8', 'computer2_2', '4');
+insert into catalog_3 values('9', 'book1_1', '5');
+insert into catalog_3 values('10','book1_2', '5');
+insert into catalog_3 values('11','book2_1', '6');
+insert into catalog_3 values('12','book2_2', '6');
