b392f00db3fe7c49241a8e886ec551091f2e79f1
Fanny <lifangning1234@yahoo.com>
2020-12-15 18:29:32 -0500

    complete search sku list from es feature : interface & impl

--- interface/src/main/java/service/ListService.java
+++ interface/src/main/java/service/ListService.java
@@ -1,9 +1,13 @@
 package service;
 
 import bean.SkuLsInfo;
+import bean.SkuLsParams;
+import bean.SkuLsResult;
+
 
 public interface ListService {
 
     public void saveSkuLsInfo(SkuLsInfo skuLsInfo);
 
+    public SkuLsResult getSkuLsInfoList(SkuLsParams skuLsParams);
 }
--- list-service/src/main/java/com/gmall/list/service/impl/ListServiceImpl.java
+++ list-service/src/main/java/com/gmall/list/service/impl/ListServiceImpl.java
@@ -2,13 +2,29 @@ package com.gmall.list.service.impl;
 
 
 import bean.SkuLsInfo;
+import bean.SkuLsParams;
+import bean.SkuLsResult;
 import com.alibaba.dubbo.config.annotation.Service;
 import io.searchbox.client.JestClient;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.search.aggregation.TermsAggregation;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import service.ListService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -26,5 +42,74 @@ public class ListServiceImpl implements ListService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public SkuLsResult getSkuLsInfoList(SkuLsParams skuLsParams) {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        // sku name search
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+        boolQueryBuilder.must(new MatchQueryBuilder("skuName", skuLsParams.getKeyword()));
+        // category search
+        boolQueryBuilder.filter(new TermQueryBuilder("catalog3Id", skuLsParams.getCatalog3Id()));
+        // sku attr filter
+        String[] valueIds = skuLsParams.getValueId();
+        for (int i = 0; i < valueIds.length; i++) {
+            String valueId = valueIds[i];
+            boolQueryBuilder.filter(new TermQueryBuilder("skuAttrValueList.valueId", valueId));
+        }
+        // price
+        boolQueryBuilder.filter(new RangeQueryBuilder("price").gte("3200"));
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        // init row
+        searchSourceBuilder.from((skuLsParams.getPageNo()-1)*skuLsParams.getPageSize());
+        searchSourceBuilder.size(skuLsParams.getPageSize());
+
+        // highlight
+        searchSourceBuilder.highlight(new HighlightBuilder().field("skuName").preTags("<span style='color:red'>").postTags("</span>"));
+
+        // aggregation
+        TermsBuilder aggsBuilder = AggregationBuilders.terms("groupby_value_id").field("skuAttrValueList.valueId").size(1000);
+        searchSourceBuilder.aggregation(aggsBuilder);
+
+        // sort
+        searchSourceBuilder.sort("hotScore", SortOrder.DESC);
+
+        Search.Builder searchBuilder = new Search.Builder(searchSourceBuilder.toString());
+        Search search = searchBuilder.addIndex("gmall_sku_info").addType("_doc").build();
+
+        // instantiate return obj
+        // encapsulate skuLsResult
+        SkuLsResult skuLsResult = new SkuLsResult();
+        try {
+            SearchResult searchResult = jestClient.execute(search);
+
+
+            // sku list
+            List<SkuLsInfo> skuLsInfoList = new ArrayList<>();
+            List<SearchResult.Hit<SkuLsInfo, Void>> hits = searchResult.getHits(SkuLsInfo.class);
+            for (SearchResult.Hit<SkuLsInfo, Void> hit : hits) {
+                SkuLsInfo skuLsInfo = hit.source;
+                skuLsInfoList.add(skuLsInfo);
+            }
+            skuLsResult.setSkuLsInfoList(skuLsInfoList);
+
+            // total number
+            Long total = searchResult.getTotal();
+            long totalPage = (total+skuLsParams.getPageSize()-1)/skuLsParams.getPageSize();
+            skuLsResult.setTotalPages(totalPage);
+
+            // aggregation part: sku attr
+            List<String> attrValueIdList=new ArrayList<String>();
+            List<TermsAggregation.Entry> buckets = searchResult.getAggregations().getTermsAggregation("groupby_value_id").getBuckets();
+            for (TermsAggregation.Entry bucket : buckets) {
+                attrValueIdList.add(bucket.getKey());
+            }
+            skuLsResult.setAttrValueIdList(attrValueIdList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return skuLsResult;
     }
 }
